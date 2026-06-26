9e3a5a3d3a7a457412a75b805afdebeafae727b0
ummedsingh <ummedsingh3062000@gmail.com>
2026-06-27 00:21:45 +0530

    first commit

---
+++ src/main/java/com/mru/psdemo/repository/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.mru.psdemo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mru.psdemo.entity.Student;
+
+public interface StudentRepo extends JpaRepository<Student, Long>{
+    
+}
